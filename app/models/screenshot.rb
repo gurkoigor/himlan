c828849ec1d68a68426209dd1b7ce09334e44657
Igor <gurko.igor@gmail.com>
2010-06-01 20:18:28 +0300

    fixed screenshot size

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -3,7 +3,7 @@ class Screenshot < ActiveRecord::Base
   
   has_attached_file :screenshot,
     :path => ":rails_root/public/system/screenshots/:id/:style/:filename",
-    :styles => {:small => "90x90#", :rectangle => "170X90#", :lightbox => "600X400#" }
+    :styles => {:small => "90x90#", :rectangle => "170X90#", :lightbox => "600X400>" }
 
   attr_accessor :delete_photo
 
